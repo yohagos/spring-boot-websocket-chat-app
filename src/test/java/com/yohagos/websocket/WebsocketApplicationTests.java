a37b3f17589d47b05d1a278a795086f1a2d7862c
yohagos <yosef.hagos@googlemail.com>
2024-03-27 15:39:37 +0100

    init project

---
+++ src/test/java/com/yohagos/websocket/WebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yohagos.websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
