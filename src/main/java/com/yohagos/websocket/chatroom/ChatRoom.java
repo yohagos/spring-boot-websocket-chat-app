bfbb7c926cc6e4137f16ec4baba41924bbc68d38
yohagos <yosef.hagos@googlemail.com>
2024-03-28 14:23:26 +0100

    fix: replaced @Data with @Getter and @Setter

--- src/main/java/com/yohagos/websocket/chatroom/ChatRoom.java
+++ src/main/java/com/yohagos/websocket/chatroom/ChatRoom.java
@@ -1,13 +1,11 @@
 package com.yohagos.websocket.chatroom;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
--- src/main/java/com/yohagos/websocket/user/User.java
+++ src/main/java/com/yohagos/websocket/user/User.java
@@ -1,13 +1,11 @@
 package com.yohagos.websocket.user;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Data
+@Getter
+@Setter
 @Builder
 @Document
 @NoArgsConstructor
