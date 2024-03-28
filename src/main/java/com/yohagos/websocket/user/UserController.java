2197de6751cc5d01b5d0796dc309ca62400aa719
yohagos <yosef.hagos@googlemail.com>
2024-03-28 14:01:19 +0100

    feat: second Part of User package

---
+++ src/main/java/com/yohagos/websocket/user/UserController.java
@@ -0,0 +1,42 @@
+package com.yohagos.websocket.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+
+    @MessageMapping("/user.addUser")
+    @SendTo("/user/topic")
+    public User add(
+            @Payload User user
+    ) {
+        userService.saveUser(user);
+        return user;
+    }
+
+    @MessageMapping("/user.disconnectUser")
+    @SendTo("/user/topic")
+    public User disconnect(
+            @Payload User user
+    ) {
+        userService.disconnectUser(user);
+        return user;
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> findConnectedUsers() {
+        return ResponseEntity.ok(userService.findConnectedUsers());
+    }
+}
