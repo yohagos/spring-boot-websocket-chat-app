36c2cf33df93190437785d714f7efb4724e6c4ac
yohagos <yosef.hagos@googlemail.com>
2024-03-28 14:20:50 +0100

    feat: implemented entity, repository and service for chatroom

---
+++ src/main/java/com/yohagos/websocket/chatroom/ChatRoomRepository.java
@@ -0,0 +1,11 @@
+package com.yohagos.websocket.chatroom;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ChatRoomRepository extends MongoRepository<ChatRoom, String> {
+    Optional<ChatRoom> findBySenderIdAndRecipientId(String senderId, String recipientId);
+}
